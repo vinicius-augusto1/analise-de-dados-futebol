2e5e276398c56742d5863c51e7e8aa4ea4665648
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-10-17 19:08:06 -0300

    fix: removendo getters e setters inutilizáveis e corrigindo formatações

--- src/model/Cartao.java
+++ src/model/Cartao.java
@@ -1,49 +1,12 @@
 package model;
 
 public class Cartao {
-    private Integer partidaId;
-    private String rodada;
-    private String clube;
     private String cartao;
     private String atleta;
-    private Integer numeroCamisa;
-    private String posicao;
-    private String minuto;
 
-    public Cartao(Integer partidaId, String rodada, String clube, String cartao, String atleta, Integer numeroCamisa, String posicao, String minuto) {
-        this.partidaId = partidaId;
-        this.rodada = rodada;
-        this.clube = clube;
-        this.cartao = cartao;
+    public Cartao(String atleta, String cartao) {
         this.atleta = atleta;
-        this.numeroCamisa = numeroCamisa;
-        this.posicao = posicao;
-        this.minuto = minuto;
-    }
-
-
-    public Integer getPartidaId() {
-        return partidaId;
-    }
-
-    public void setPartidaId(Integer partidaId) {
-        this.partidaId = partidaId;
-    }
-
-    public String getRodada() {
-        return rodada;
-    }
-
-    public void setRodada(String rodada) {
-        this.rodada = rodada;
-    }
-
-    public String getClube() {
-        return clube;
-    }
-
-    public void setClube(String clube) {
-        this.clube = clube;
+        this.cartao = cartao;
     }
 
     public String getCartao() {
@@ -62,27 +25,4 @@ public class Cartao {
         this.atleta = atleta;
     }
 
-    public Integer getNumeroCamisa() {
-        return numeroCamisa;
-    }
-
-    public void setNumeroCamisa(Integer numeroCamisa) {
-        this.numeroCamisa = numeroCamisa;
-    }
-
-    public String getPosicao() {
-        return posicao;
-    }
-
-    public void setPosicao(String posicao) {
-        this.posicao = posicao;
-    }
-
-    public String getMinuto() {
-        return minuto;
-    }
-
-    public void setMinuto(String minuto) {
-        this.minuto = minuto;
-    }
 }

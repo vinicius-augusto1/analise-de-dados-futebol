be559abaa10d0136b6d52b2cd0eb71caf4c46089
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-10-19 08:55:15 -0300

    adicionando estização na chamada dos serviços

--- src/controller/CampeonatoController.java
+++ src/controller/CampeonatoController.java
@@ -1,36 +1,34 @@
 package controller;
 
+import repository.PartidaRepository;
 import service.CampeonatoService;
+import utils.MenuEstilizado;
 
+import java.awt.*;
 import java.io.IOException;
+
+import static utils.MenuEstilizado.*;
 
 public class CampeonatoController {
 
     private CampeonatoService service;
 
-    public CampeonatoController(String jogadorFile, String partidasFile, String timesFile, String cartaoFile) throws IOException{
+
+
+    public CampeonatoController(String jogadorFile, String partidasFile, String timesFile, String cartaoFile) throws IOException {
         this.service = new CampeonatoService(jogadorFile, partidasFile, timesFile, cartaoFile);
     }
 
 
+
     public void exibirEstatisticas() throws IOException {
-//        System.out.println("Time que mais venceu em 2008: " + service.timeMaisVenceu2008());
-//        System.out.println("Estado que teve menos jogos entre 2003 e 2022: " + service.estadoComMenosJogos());
-//        System.out.println("O jogador que mais fez gols: " + service.jogadorComMaisGols());
-//        System.out.println("O jogador que mais fez gols de penalti: " + service.jogadorComMaisGolsDePenalti());
-//        System.out.println("O jogoador que mais fez gols contra: " + service.jogadorComMaisGolsContra());
-//        System.out.println("O jogador com mais cartões amarelos: " + service.jogadorComMaisCartoesAmarelos());
-//        System.out.println("O jogador com mais cartões vermelhos: " + service.jogadorComMaisCartoesVermelhos());
-//        System.out.println("O placar da partida com mais gols: " + service.partidaComMaisGols());
-
-        System.out.println("Mais 2008 --> "+service.timeMaisVenceu2008() + "  FUNCIONA, MAS TEM QUE RESOLVER ESSE BUG");
-        System.out.println("Menos 2003 a 2022 --> "+service.estadoComMenosJogos() + "  ESTÁ OK, MAS TEM ALGUMAS FEATURES");
-        System.out.println("+ Gols --> "+service.jogadorComMaisGols());
-        System.out.println("+ Penaltis --> "+service.jogadorComMaisGolsDePenalti());
-        System.out.println("+ Contra --> "+service.jogadorComMaisGolsContra());
-        System.out.println("+ Amarelos  --> "+service.jogadorComMaisCartoesAmarelos() + "  ESTÁ OK");
-        System.out.println("+ Vermelhos --> "+ service.jogadorComMaisCartoesVermelhos() + "  ESTÁ OK");
-        System.out.println("+ Gols Partida --> "+service.partidaComMaisGols() + "  ESTÁ OK");
+        MenuEstilizado.printStylizedMessage(service.timeMaisVenceu2008(), ANSI_GREEN);
+        MenuEstilizado.printStylizedMessage(service.estadoComMenosJogos(), ANSI_YELLOW);
+        MenuEstilizado.printStylizedMessage("Mais gols: " + service.jogadorComMaisGols(), ANSI_BLUE);
+        MenuEstilizado.printStylizedMessage("Mais gols de pênalti: " + service.jogadorComMaisGolsDePenalti(), ANSI_RED);
+        MenuEstilizado.printStylizedMessage("Mais gols contra: " + service.jogadorComMaisGolsContra(), ANSI_YELLOW);
+        MenuEstilizado.printStylizedMessage("Mais cartões amarelos: " + service.jogadorComMaisCartoesAmarelos(), ANSI_GREEN);
+        MenuEstilizado.printStylizedMessage("Mais cartões vermelhos: " + service.jogadorComMaisCartoesVermelhos(), ANSI_RED);
+        MenuEstilizado.printStylizedMessage("O placar da partida com mais gols: " + service.partidaComMaisGols(), ANSI_BLUE);
     }
-
 }
